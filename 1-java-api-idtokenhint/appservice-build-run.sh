40feb50c630f2804e947bdc4a1c5f87ee19fe56b
Christer Ljung <cljung@live.se>
2024-07-11 11:13:53 +0200

    add missing png

--- 1-java-api-idtokenhint/appservice-build-run.sh
+++ 1-java-api-idtokenhint/appservice-build-run.sh
@@ -17,5 +17,9 @@ if [ ! -f $wwwdir/app.jar ]; then
   mvn package -DskipTests
   cp target/*.jar $wwwdir/app.jar
 fi
+# Set apiKey if not set
+if [[ ! -z "${VerifiedID__apiKey}" ]]; then
+  export VerifiedID__apiKey=$WEBSITE_INSTANCE_ID
+fi
 cd $wwwdir
 java -jar ./app.jar
